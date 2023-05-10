bff0999fb1e55b94f0ff9159172ca00e4febd3f8
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-05-10 14:39:04 +0300

    Scripts for reconstruction & sync. WIP.

--- scripts/reconstruct.sh
+++ scripts/reconstruct.sh
@@ -1,41 +1,69 @@
+if [ -z "$MX_NETWORK" ]
+then
+    echo "Environment variable isn't defined: MX_NETWORK."
+    exit 1
+fi
 
+if [ -z "$MX_SHARD" ]
+then
+    echo "Environment variable isn't defined: MX_SHARD."
+    exit 1
+fi
+
+if [ -z "$MX_WORKSPACE_ROOT" ]
+then
+    echo "Environment variable isn't defined: MX_WORKSPACE_ROOT."
+    exit 1
+fi
+
+MX_WORKSPACE=$MX_WORKSPACE_ROOT/$MX_NETWORK/$MX_SHARD
+
+# Downloads (and extracts) a regular, "full-history", daily archive.
 download_daily_archive() {
-    mkdir -p $WORKSPACE/daily_archive
-
-    echo "Downloading archive ..."
-    wget -O $WORKSPACE/daily_archive/archive $URL_DAILY_ARCHIVE || return 1
-
-    echo "Extracting archive" $WORKSPACE/daily_archive/archive "..."
-    tar -xf  $WORKSPACE/daily_archive/archive -C $WORKSPACE/daily_archive || return 1
-}
-
-prepare_import_db() {
-    mkdir -p $WORKSPACE/data/db
-    mkdir -p $WORKSPACE/data/import-db
-    mv $WORKSPACE/daily_archive/db $WORKSPACE/data/import-db/ || return 1
-}
-
-run_import_db() {
-    if [ ! -d "${WORKSPACE}/data/import-db" ] 
+    if [ -z "$MX_URL_DAILY_ARCHIVE" ]
     then
-        echo "Error: ${WORKSPACE}/data/import-db does not exist. Please run prepare_import_db first." 
+        echo "Environment variable isn't defined: MX_URL_DAILY_ARCHIVE."
         return 1
     fi
 
-    docker run --rm -it \
+    mkdir -p $MX_WORKSPACE/daily_archive
+
+    echo "Downloading archive ..."
+    wget -O $MX_WORKSPACE/daily_archive/archive $URL_DAILY_ARCHIVE || return 1
+
+    echo "Extracting archive" $MX_WORKSPACE/daily_archive/archive "..."
+    tar -xf  $MX_WORKSPACE/daily_archive/archive -C $MX_WORKSPACE/daily_archive || return 1
+}
+
+# Prepares the import-db folder (using the data extracted from the daily archive).
+prepare_import_db() {
+    mkdir -p $MX_WORKSPACE/data/db
+    mkdir -p $MX_WORKSPACE/data/import-db
+    mv $MX_WORKSPACE/daily_archive/db $MX_WORKSPACE/data/import-db/ || return 1
+}
+
+# Runs the import-db process (using Docker).
+run_import_db() {
+    if [ ! -d "${MX_WORKSPACE}/data/import-db" ] 
+    then
+        echo "Error: ${MX_WORKSPACE}/data/import-db does not exist. Please run prepare_import_db first." 
+        return 1
+    fi
+
+    docker run \
         --user $(id -u):$(id -g) \
-        --name reconstruct-${NETWORK}-${SHARD} \
-        --volume ${WORKSPACE}/data:/data \
-        --env LD_LIBRARY_PATH=/${NETWORK}/node \
-        --workdir /${NETWORK}/node \
-        --entrypoint /${NETWORK}/node/node \
+        --name deep-history-reconstruct-${MX_NETWORK}-${MX_SHARD} \
+        --volume ${MX_WORKSPACE}/data:/data \
+        --env LD_LIBRARY_PATH=/${MX_NETWORK}/node \
+        --workdir /${MX_NETWORK}/node \
+        --entrypoint /${MX_NETWORK}/node/node \
         multiversx/deep-history:latest \
         --working-directory=/data \
         --log-save \
         --log-level=*:INFO \
         --log-logger-name \
         --rest-api-interface=0.0.0.0:8080 \
-        --destination-shard-as-observer=${SHARD} \
+        --destination-shard-as-observer=${MX_SHARD} \
         --import-db=/data/import-db \
         --import-db-no-sig-check || return 1
 }
---
+++ scripts/sync.sh
@@ -0,0 +1,43 @@
+if [ -z "$MX_NETWORK" ]
+then
+    echo "Environment variable isn't defined: MX_NETWORK."
+    exit 1
+fi
+
+if [ -z "$MX_SHARD" ]
+then
+    echo "Environment variable isn't defined: MX_SHARD."
+    exit 1
+fi
+
+if [ -z "$MX_WORKSPACE_ROOT" ]
+then
+    echo "Environment variable isn't defined: MX_WORKSPACE_ROOT."
+    exit 1
+fi
+
+MX_WORKSPACE=$MX_WORKSPACE_ROOT/$MX_NETWORK/$MX_SHARD
+
+# Runs the observer (using Docker).
+run_observer() {
+    if [ ! -d "${MX_WORKSPACE}/data/db" ] 
+    then
+        echo "Error: ${MX_WORKSPACE}/data/db does not exist. Perhaps it hasn't been reconstructed yet?" 
+        return 1
+    fi
+
+    docker run \
+        --user $(id -u):$(id -g) \
+        --name deep-history-observer-${MX_NETWORK}-${MX_SHARD} \
+        --volume ${MX_WORKSPACE}/data:/data \
+        --env LD_LIBRARY_PATH=/${MX_NETWORK}/node \
+        --workdir /${MX_NETWORK}/node \
+        --entrypoint /${MX_NETWORK}/node/node \
+        multiversx/deep-history:latest \
+        --working-directory=/data \
+        --log-save \
+        --log-level=*:INFO \
+        --log-logger-name \
+        --rest-api-interface=0.0.0.0:8080 \
+        --destination-shard-as-observer=${MX_SHARD} || return 1
+}
